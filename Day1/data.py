be521737086e5903d0ec1fe8fa9fb91d43be48ba
MHThomsen <mortenthomsen4@gmail.com>
2021-06-04 10:16:33 +0200

    fixed printline

--- Day1/data.py
+++ Day1/data.py
@@ -5,7 +5,6 @@ import os
 def mnist():
     download=True
     if "MNIST" in os.listdir():
-        print("Downloading data :) ")
         download = False
     
     train = MNIST("./", train=True, download=download)
