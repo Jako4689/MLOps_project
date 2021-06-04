9c228c009f7a20fde832f1e1f0523427c0ceeaa0
MHThomsen <mortenthomsen4@gmail.com>
2021-06-04 10:13:26 +0200

    downloads data

--- Day1/data.py
+++ Day1/data.py
@@ -1,8 +1,15 @@
 from torchvision.datasets import MNIST
 
+import os
+
 def mnist():
-    train = MNIST("./", train=True, download=False)
-    test = MNIST("./", train=False, download=False)
+    download=True
+    if "MNIST" in os.listdir():
+        print("Downloading data :) ")
+        download = False
+    
+    train = MNIST("./", train=True, download=download)
+    test = MNIST("./", train=False, download=download)
     
     
     return train, test
