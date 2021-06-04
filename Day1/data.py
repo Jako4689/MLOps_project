ea83ef1102b2c51a39bec3d28cbdb4f1d8197aab
MHThomsen <mortenthomsen4@gmail.com>
2021-06-04 10:42:59 +0200

    transforms to data

--- Day1/data.py
+++ Day1/data.py
@@ -1,18 +1,23 @@
 from torchvision.datasets import MNIST
+from torchvision import transforms
 
 import os
 
-def mnist():
+def mnist(transform = True):
     download=True
     if "MNIST" in os.listdir():
+        print("Data already downloaded.")
         download = False
+
+    if transform:
+        transformations = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5,), (0.5,)),
+                              ])
     
-    train = MNIST("./", train=True, download=download)
-    test = MNIST("./", train=False, download=download)
-    
+    train = MNIST("./", train=True, download=download,transforms=transformations)
+    test = MNIST("./", train=False, download=download,transforms=transformations)
     
     return train, test
-
 
 if __name__ == '__main__':
     mnist()
