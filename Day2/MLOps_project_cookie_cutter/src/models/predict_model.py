ac6b374a62aaa3dd0bd18c46002223e3abe2703b
MHThomsen <mortenthomsen4@gmail.com>
2021-06-07 11:27:54 +0200

    exercise 4.4. Not implemented but structure in place

---
+++ Day2/MLOps_project_cookie_cutter/src/models/predict_model.py
@@ -0,0 +1,26 @@
+
+
+def main():
+    """ 
+    Docs
+    """
+    raise NotImplementedError("Will be implemented when there is a usecase")
+
+
+if __name__ == '__main__':
+    main()
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
